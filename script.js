181e72237a7ba84e1a344fd7d21b9047cec13540
Adam Ross Benton <adamrossbenton@gmail.com>
2021-08-27 15:31:41 -0700

    Set up js constants

---
+++ script.js
@@ -0,0 +1,12 @@
+// API: https://api.magicthegathering.io/v1/cards
+
+const $name = $('#name')
+const $manaCost = $('#mana-cost')
+const $colors = $('#colors')
+const $type = $('#types')
+const $subtype = $('#subtypes')
+const $rarity = $('#rarity')
+const $text = $('#text')
+const $power = $('#power')
+const $toughness = $('#toughness')
+const $image = $('#image')
