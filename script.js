db960720ebf33564d3ae2be979247a433c6206a5
Adam Ross Benton <adamrossbenton@gmail.com>
2021-08-27 16:08:47 -0700

    start to set up js

--- script.js
+++ script.js
@@ -10,4 +10,23 @@ const $text = $('#text')
 const $power = $('#power')
 const $toughness = $('#toughness')
 const $image = $('#image')
+const getPack = $('#get-pack')
 
+let cardDatabase
+
+function handleGetData(event){
+    event.preventDefault()
+
+    $.ajax({
+        url: https://api.magicthegathering.io/v1/cards
+    }).then(
+        data => {
+
+        },
+        error => {
+
+        }
+    )
+}
+
+$getPack.on('click',handleGetData)
